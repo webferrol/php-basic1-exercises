9d93627da0451c199e0cfdab6f44c0fa2a86701c
Rodrigo Sambade <rodrigosambadesaa@gmail.com>
2024-02-12 10:37:35 +0100

    Update script1.php

--- script1.php
+++ script1.php
@@ -1,6 +1,6 @@
 <?php
 //1. Crear una estructura html y mostrar en el título <title> el saludo "Hola tu nombre". El nombre debe aparecer mediante una variable
-$nombre = 'Mi nombre';
+$nombre = 'Rodrigo Sambade Saá';
 ?>
 <!DOCTYPE html>
 <html lang="es">
